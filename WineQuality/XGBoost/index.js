a8ad77ae31004fb4ed5ea3fbf54018f87c92f6fa
singhAbhiPNW <146111180+singhAbhiPNW@users.noreply.github.com>
2023-10-16 19:42:54 -0500

    Update index.js

--- WineQuality/XGBoost/index.js
+++ WineQuality/XGBoost/index.js
@@ -1,33 +1,38 @@
-async function runExample(){
-  var x = [];
+async function runExample() {
 
-  x[0] = document.getElementById('box1').value;
-  x[1] = document.getElementById('box2').value;
-  x[2] = document.getElementById('box3').value;
-  x[3] = document.getElementById('box4').value;
-  x[4] = document.getElementById('box5').value;
-  x[5] = document.getElementById('box6').value;
-  x[6] = document.getElementById('box7').value;
-  x[7] = document.getElementById('box8').value;
-  x[8] = document.getElementById('box9').value;
-  x[9] = document.getElementById('box10').value;
-  x[10] = document.getElementById('box11').value;
+    var x = new Float32Array( 1, 11 )
 
-  let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
+    var x = [];
 
-  let session = new onnx.InferenceSession();
+     x[0] = document.getElementById('box1').value;
+     x[1] = document.getElementById('box2').value;
+     x[2] = document.getElementById('box3').value;
+     x[3] = document.getElementById('box4').value;
+     x[4] = document.getElementById('box5').value;
+     x[5] = document.getElementById('box6').value;
+     x[6] = document.getElementById('box7').value;
+     x[7] = document.getElementById('box8').value;
+     x[8] = document.getElementById('box9').value;
+     x[9] = document.getElementById('box10').value;
+     x[10] = document.getElementById('box11').value;
 
-  await session.loadModel("DLnet_WineData.onnx");
-  let outputMap = await session.run([tensorX]);
-  let outputData = outputMap.get('result');
+    let tensorX = new ort.Tensor('float32', x, [1, 11] );
+    let feeds = {float_input: tensorX};
 
-  let predictions = document.getElementById("predictions");
-  predictions.innerHTML = `<hr> Got an output tensor with value: <br/>
-  <table>
-  <tr>
-    <td> Rating of Wine Quality</td>
-    <td id="qulaity">${outputData.data[0].toFixed(2)}</td>
-  </tr>
-  </table>
-  `;
+    let session = await ort.InferenceSession.create('xgboost_WineQuality_ort.onnx');
+    
+   let result = await session.run(feeds);
+   let outputData = result.variable.data;
+
+  outputData = parseFloat(outputData).toFixed(2)
+
+   let predictions = document.getElementById('predictions');
+
+  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+   <table>
+     <tr>
+       <td>  Rating of Wine Quality  </td>
+       <td id="td0">  ${outputData}  </td>
+     </tr>
+  </table>`;
 }
